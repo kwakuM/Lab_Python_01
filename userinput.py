e4f28e95e600e81f376d1786637fae668ff86b8c
Ananse <ananse@ananse-laptop.(none)>
2012-06-20 17:07:01 +0000

    answers tolab 1

---
+++ userinput.py
@@ -0,0 +1,7 @@
+firstname=raw_input("Enter your firstname:")
+lastname=raw_input("Enter your Lastname:")
+print "Enter your date of birth"
+month=raw_input("month?")
+Day=raw_input("Day?")
+Year=raw_input("Year?")
+print firstname +' '+ lastname +' '+ 'was born on '+' '+month+' '+ Day +','+Year
